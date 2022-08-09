ca0a7929411dca0f7ad5482ba2f4f45ad053c8bb
Pavel_Budantsev <p.budantsev@ya.ru>
2022-08-09 12:49:41 +0500

    Три задачи на проверку

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine("Введите трехзначное число");
+// int threeDigit = Convert.ToInt32(Console.ReadLine());
+// int secondNumber = ((threeDigit % 100) / 10);
+// Console.WriteLine($"Вторая цифра этого числа: {secondNumber}");
+
+// Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+// Console.WriteLine("Введите натуральное число");
+// string numberAsString = (Console.ReadLine());
+// if (numberAsString.Length > 2)
+// {
+//     Console.WriteLine ($"Третья цифра заданного числа - это {numberAsString [2]}");
+// }
+// else Console.WriteLine ("В этом числе нет третьей цифры");
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+// Console.WriteLine("Введите номер дня недели от 1 до 7 включительно");
+// int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+// if (dayOfWeek == 6 | dayOfWeek == 7)
+// {
+//     Console.WriteLine("Этот день недели - выходной");
+// }
+// else Console.WriteLine("Этот день недели точно не выходной");
